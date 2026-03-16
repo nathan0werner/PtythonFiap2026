410de40caa62aceff9367aafa296b0f17e229275
nathan0werner <nathanwerner4@gmail.com>
2026-03-16 09:53:16 -0300

    aula 02 - variaveis

---
+++ aula02a-variaveis.py
@@ -0,0 +1,47 @@
+print ("Olá, mundo")
+
+print (7+4)
+
+print ('7'+ '4') #CONCATENANDO STRINGS
+
+
+
+nome = "Nathan"
+idade = 26
+peso = 86.3
+
+print(nome, idade, peso)
+
+# INPUT - UTILIZADO PARA RECEBER DADOS DO USUÁRIO
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade:")
+peso =input("Digite seu peso:")
+
+print(nome, idade, peso)
+
+
+
+
+
+
+
+
+nome= input("Digite seu nome:")
+print (f"Oii, Seja Bem-Vindo {nome}")
+
+dia = input("Digite seu dia de nascimento:")
+mês = input("Digite o mês do seu nascimento:")
+ano = input("Digite o ano do seu nascimento:")
+print(dia, mês, ano)
+
+print (70 + 13)
+
+
+
+
+
+
+
+
+
